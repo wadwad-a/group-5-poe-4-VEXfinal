b3b7f99ddbc014f653d7322e6bcfee5543fff702
daSeaTurtle <aarondsouz922forrealz@gmail.com>
2026-06-01 14:01:51 -0400

    Set up Vex V5 project
    - created vex v5 project under the repository
    - updated authors and description in the header

---
+++ dsouza-vex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Aaron D'Souza & Aaron Okon                                   #
+# 	Created:      6/1/2026, 1:59:21 PM                                         #
+# 	Description:  Code for VEX RECbot final                                    #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
