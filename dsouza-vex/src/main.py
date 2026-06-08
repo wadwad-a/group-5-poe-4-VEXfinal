a5427ce3337c5cbf10b19e5eeea9365357847cf5
daSeaTurtle <aarondsouz922forrealz@gmail.com>
2026-06-08 13:57:23 -0400

    Final Drive Sequence v1
    - added the rest of the functions in main() for the sequence to complete
    - adjusted some of the values

--- dsouza-vex/src/main.py
+++ dsouza-vex/src/main.py
@@ -296,12 +296,18 @@ def main():
 
     driveStraight(94, 0, 90)        # drive forward to where the ball is located
     liftArm(20, 50)                 # lift the arm to grab the ball
-    driveStraight(10, 0, -40)       # drive backward to align the position with the next turn
-    pointTurn(90)                   # turn 90 degrees to the right to face the goal
-    driveStraight(65, 0, 90)        # drive forward to the goal
-    pointTurn(45)                   # turn 45 degrees to the right to align with the goal
-    driveStraight(12, 0, 90)        # drive forward to the goal
-    liftArm(20, -50)
+    driveStraight(13, 0, -40)       # drive backward to align the position with the next turn
+    pointTurn(90)                   # turn to face the goal
+    driveStraight(65, 0, 80)        # drive forward to the goal
+    pointTurn(45)                   # turn to align with the goal
+    driveStraight(14, 0, 90)        # drive forward to the goal
+    wait(0.5, SECONDS)              # wait for ball to settle
+    liftArm(20, -50)                # lower the arm to release the ball
+    liftArm(20, 100)                # lift arm to a higher position for parking sequence
+    pointTurn(145)                  # turn to get into position for parking
+    driveStraight(25, 0, -90)       # drive backward to align with the parking spot
+    pointTurn(90)                   # turn to align with the parking spot
+    driveStraight(36, 0, -90)       # drive backward into parking spot
 # ----------------------------------------------------------------------------------------
 
 main()
